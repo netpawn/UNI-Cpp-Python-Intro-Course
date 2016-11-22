6d46cd41c070783dd2dcda3785279dfd17a32042
Tommaso Pellegrini <tppellegrini@gmail.com>
2016-11-22 19:26:04 +0100

    Update MARIOnew - Copia.py
    Fixed a lot of things, added luigi. Need death

--- MARIOnew - Copia.py
+++ MARIOnew - Copia.py
@@ -33,36 +33,33 @@ class Jumper(Actor):
         self._x += self._dx       
         if self._x < 0:
             self._x = 0
-        elif self._x > arena_w - self.W:
+        elif self._x > arena_w - self.W: 
             self._x = arena_w - self.W
 
     
     def jump(self):
-        if self._landed:
-            self._synX, self._synY = 355, 0
-            self._dy = -self.MAX_SPEED
-            self._landed = False
+        pass
             
     def go_left(self):
-        self._synX, self._synY = 175, 0
-        self._dx = -self.SPEED
-        
+        pass
+    
     def go_right(self):
-        self._synX, self._synY = 210, 0
-        self._dx = self.SPEED
+        pass
 
     def stay(self):
-        self._synX, self._synY = 210, 0
-        self._dx = 0
+        pass
 
     def collide(self, other):
         
-##        if isinstance(other, Goomba):
-##            self._death = True
-##            arena.remove(Mario)
+        if isinstance(other, Goomba):
+            bx, by, bw, bh = self.rect()  # actor's pos
+            wx, wy, ww, wh = other.rect() # enemy's pos
+            borders_distance = [(wx - bw - bx, 0), (wx + ww - bx, 0),
+                                (0, wy - bh - by), (0, wy + wh - by)]
+            arena.remove(self)
         
         if isinstance(other, Wall):
-            bx, by, bw, bh = self.rect()  # ball's pos
+            bx, by, bw, bh = self.rect()  # actor's pos
             wx, wy, ww, wh = other.rect() # wall's pos
             borders_distance = [(wx - bw - bx, 0), (wx + ww - bx, 0),
                                 (0, wy - bh - by), (0, wy + wh - by)]
@@ -107,15 +104,73 @@ class Jumper(Actor):
         return self._synX, self._synY
 
 class Mario(Jumper):
-    pass
 
-class Goomba(Actor):
-    W, H = 20, 20
-    SPEED = 2
+    def jump(self): #add boolean for mario in class 
+        if self._landed:
+            if self._dx >= 0:
+                self._synX, self._synY = 355, 0
+
+            elif self._dx < 0:
+                self._synX, self._synY = 28, 0
+            
+            self._dy = -self.MAX_SPEED
+            self._landed = False
+            
+    def go_left(self):
+        self._synX, self._synY = 175, 0
+        self._dx = -self.SPEED
+        
+    def go_right(self):
+        self._synX, self._synY = 210, 0
+        self._dx = self.SPEED
+
+    def stay(self):
+        if self._dx > 0:
+            self._synX, self._synY = 210, 0
+            
+        elif self._dx < 0:
+            self._synX, self._synY = 175, 0
+
+        self._dx = 0   
+
+class Luigi(Jumper):
+    
+    def jump(self):
+        if self._landed:
+            if self._dx >= 0:
+                self._synX, self._synY = 355, 188
+
+            elif self._dx < 0:
+                self._synX, self._synY = 28, 188
+        
+            
+            self._dy = -self.MAX_SPEED
+            self._landed = False
+            
+    def go_left(self):
+        self._synX, self._synY = 175, 188
+        self._dx = -self.SPEED
+        
+    def go_right(self):
+        self._synX, self._synY = 210, 188
+        self._dx = self.SPEED
+
+    def stay(self):
+        if self._dx > 0:
+            self._synX, self._synY = 210, 188
+            
+        elif self._dx < 0:
+            self._synX, self._synY = 175, 188
+
+        self._dx = 0    
+
+class Enemy(Actor):
+    W, H = 15, 15
+    SPEED = 4
 
     def __init__(self, arena, x, y, synX, synY):
         self._x, self._y = x, y
-        self._dx, self._dy = 0, 0
+        self._dx, self._dy = 2, 0
         self._synX, self._synY = synX, synY
         self._landed = False
         self._arena = arena
@@ -124,15 +179,20 @@ class Goomba(Actor):
     def move(self):
         arena_w, arena_h = self._arena.size()
         
-        r = randrange(30)
-        if r == 0:
-            self.go_left()
+        r = randrange(20)
+    
+        self._x += self._dx
+        if self._x < 0:
+            self._x = -self._x
+        elif self._x > arena_w - self.W: 
+            self._x = arena_w - self.W
+        
         elif r == 1:
-            self.go_right()
+            self._dx = -self._dx
 
     def collide(self, other):
         if isinstance(other, Wall):
-            bx, by, bw, bh = self.rect()  # ball's pos
+            bx, by, bw, bh = self.rect()  # actor's pos
             wx, wy, ww, wh = other.rect() # wall's pos
             borders_distance = [(wx - bw - bx, 0), (wx + ww - bx, 0),
                                 (0, wy - bh - by), (0, wy + wh - by)]
@@ -146,6 +206,7 @@ class Goomba(Actor):
                 self._dy = 5
                 self._landed = True
 
+
     def jump(self):
         pass
             
@@ -154,7 +215,7 @@ class Goomba(Actor):
         
     def go_right(self):
         pass
-
+        
     def stay(self):
         pass
        
@@ -164,8 +225,11 @@ class Goomba(Actor):
     def symbol(self):
         return self._synX, self._synY
 
+class Goomba(Enemy):
+    pass
 
-class Wall(Actor):
+
+class Wall():
 
     def __init__(self, arena, x, y, w, h, synX, synY):
         self._x, self._y = x, y
@@ -188,20 +252,19 @@ class Wall(Actor):
 
 
 def update():
-    canvas_fill(canvas, (200, 200, 255))
-
+    image_blit(canvas, background, (0, 0))
+    
     for a in walls:
         x, y, w, h = a.rect()
         xs, ys = a.symbol()
         image_blit(canvas, sprites, (x, y), area = (xs, ys, w, h))
-        
-    arena.move_all()
     
     for a in arena.actors():
         x, y, w, h = a.rect()
         xs, ys = a.symbol()
-        a_symbol = image_blit(canvas, sprites, (x, y), area = (xs, ys, w, h))
+        image_blit(canvas, sprites, (x, y), area = (xs, ys, w, h))
 
+    arena.move_all()
 
 def print_arena(arena):
     for a in arena.actors():
@@ -209,24 +272,33 @@ def print_arena(arena):
 
 
 def keydown(event):
-    code = event.code
-    if code == "ArrowRight":
+    code = event.keyCode
+    if code == 39:
         mario.go_right()
-    elif code == "ArrowLeft":
+    elif code == 37:
         mario.go_left()
-    elif code == "Space":
+    elif code == 38:
         mario.jump()
-
+    elif code == 68:
+        luigi.go_right()
+    elif code == 65:
+        luigi.go_left()
+    elif code == 87:
+        luigi.jump()
+        
 def keyup(event):
-    code = event.code
-    if code in ("ArrowLeft", "ArrowRight"):
+    code = event.keyCode
+    if code in (37, 39):
         mario.stay()
+    if code in (65, 68):
+        luigi.stay()
 
 
-arena = Arena(300, 240)
+arena = Arena(995, 240)
 
 mario = Mario(arena, 10, 10, 210, 0)
-goomba = Goomba(arena, 150, 214, 0, 380)
+luigi = Luigi(arena, 20, 10, 210, 188)
+goomba = Goomba(arena, 100, 225, 0, 380)
 #---------------------------------------------
 walls = []
 wall = Wall(arena, 150, 160, 20, 20, 5, 640)
@@ -235,13 +307,15 @@ tubo = Wall(arena, 240, 197, 35, 70, 309, 1167)
 walls.append(tubo)
 terrain1 = Wall(arena, 0, 230, 120, 10, 4, 889)
 walls.append(terrain1)
-terrain2 = Wall(arena, 120, 230, 120, 10, 4, 889)
+terrain1 = Wall(arena, 0, 230, 120, 10, 4, 889)
+walls.append(terrain1)
+terrain2 = Wall(arena, 121, 230, 120, 10, 4, 889)
 walls.append(terrain2)
-terrain3 = Wall(arena, 240, 230, 120, 10, 4, 889)
-walls.append(terrain3)
+
 #----------------------------------------------
 canvas = canvas_init((arena.size()))
 sprites = image_load("smb_sprites.png")
+background = image_load("background.png")
 
 set_interval(update, 1000 // 30)
 
